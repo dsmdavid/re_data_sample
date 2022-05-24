34f9635dff750b16aeb745bea7b98d219f8952e1
dsmdavidIL <61737063+dsmdavidIL@users.noreply.github.com>
2022-05-25 00:17:23 +0100

    CI

--- ci.sh
+++ ci.sh
@@ -1,10 +1,25 @@
 cd weather
-echo catting
-cat /root/.ssh/rsa_key_david_sf.p8
-echo end
+echo "--- deps start ---"
 dbt deps
+echo "--- deps end ---"
+echo "\n\n\n\n\n"
+echo "--- run-operation create-artifact start ---"
 dbt run-operation create_artifact_resources
+echo "--- run-operation create-artifact end ---"
+echo "\n\n\n\n\n"
+echo "\n\n\n\n\n"
+echo "\n\n\n\n\n"
+echo "--- run main start ---"
 dbt run --exclude package:re_data package:dbt_artifacts
+echo "--- run main end ---"
+echo "\n\n\n\n\n"
+echo "\n\n\n\n\n"
+echo "\n\n\n\n\n"
+echo "--- run upload artifacts start ---"
 dbt --no-write-json run-operation upload_dbt_artifacts_v2
-dbt run --select package:re_data
+echo "\n\n\n\n\n"
+echo "--- run re_data & artifacts ---"
+dbt run --select package:re_data package:dbt_artifacts
+echo "\n\n\n\n\n"
+echo "--- run upload artifacts (II) start ---"
 dbt --no-write-json run-operation upload_dbt_artifacts_v2

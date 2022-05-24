3e1b96251f05e029284216374debbb2e1e3fcc1d
dsmdavidIL <61737063+dsmdavidIL@users.noreply.github.com>
2022-05-25 00:24:29 +0100

    CI

--- ci.sh
+++ ci.sh
@@ -1,25 +1,27 @@
 cd weather
+echo "is it bash?"
+echo $O
 echo "--- deps start ---"
 dbt deps
 echo "--- deps end ---"
-echo "\n\n\n\n\n"
+echo -ne "\n\n\n\n\n"
 echo "--- run-operation create-artifact start ---"
 dbt run-operation create_artifact_resources
 echo "--- run-operation create-artifact end ---"
-echo "\n\n\n\n\n"
-echo "\n\n\n\n\n"
-echo "\n\n\n\n\n"
+echo -ne "\n\n\n\n\n"
+echo -ne "\n\n\n\n\n"
+echo -ne "\n\n\n\n\n"
 echo "--- run main start ---"
 dbt run --exclude package:re_data package:dbt_artifacts
 echo "--- run main end ---"
-echo "\n\n\n\n\n"
-echo "\n\n\n\n\n"
-echo "\n\n\n\n\n"
+echo -ne "\n\n\n\n\n"
+echo -ne "\n\n\n\n\n"
+echo -ne "\n\n\n\n\n"
 echo "--- run upload artifacts start ---"
 dbt --no-write-json run-operation upload_dbt_artifacts_v2
-echo "\n\n\n\n\n"
+echo -ne "\n\n\n\n\n"
 echo "--- run re_data & artifacts ---"
 dbt run --select package:re_data package:dbt_artifacts
-echo "\n\n\n\n\n"
+echo -ne "\n\n\n\n\n"
 echo "--- run upload artifacts (II) start ---"
 dbt --no-write-json run-operation upload_dbt_artifacts_v2

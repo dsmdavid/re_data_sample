417a4754483417f891db3ea8fc46bfb3dc76f76e
dsmdavidIL <61737063+dsmdavidIL@users.noreply.github.com>
2022-05-25 00:09:05 +0100

    ci

--- ci.sh
+++ ci.sh
@@ -1,4 +1,7 @@
 cd weather
+echo catting
+cat /root/.ssh/rsa_key_david_sf.p8
+echo end
 dbt deps
 dbt run-operation create_artifact_resources
 dbt run --exclude package:re_data package:dbt_artifacts
